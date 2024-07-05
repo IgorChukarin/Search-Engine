39f66cbb48ba8016c63b3245c13ec538a7d4688e
IgorChukarin <Zigor2356@mail.ru>
2024-07-06 00:53:21 +0300

    add Page, Site in model

---
+++ src/main/java/searchengine/model/Page.java
@@ -0,0 +1,35 @@
+package searchengine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "page")
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "site_id", nullable = false)
+    private Site site;
+
+    @Column(name = "path", nullable = false, length = 255)
+    private String path;
+
+    @Column(name = "code", nullable = false)
+    private Integer code;
+
+    @Column(name = "content", columnDefinition = "MEDIUMTEXT", nullable = false)
+    private String content;
+}
---
+++ src/main/java/searchengine/model/Site.java
@@ -0,0 +1,43 @@
+package searchengine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "site")
+public class Site {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private SiteStatus status;
+
+    @Column(name = "status_time", nullable = false)
+    private LocalDateTime statusTime;
+
+    @Column(name = "last_error", columnDefinition = "TEXT")
+    private String lastError;
+
+    @Column(name = "url", nullable = false, length = 255)
+    private String url;
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+
+    @OneToMany(mappedBy = "site", cascade = CascadeType.ALL)
+    private List<Page> pages;
+}
